1836b13263d3d1cebce66d87999017cfdcf118d5
Hugo <hugopalominogalindo974@gmail.com>
2025-05-01 23:42:20 -0500

    ejercicios con javascript

---
+++ estructura_de_Datos/ExercisesJs/index.js
@@ -0,0 +1,6 @@
+/**
+1) Realizar una funcion que nos permita realizar
+las 4 operaciones aritmeticas, es decir nos permita
+sumar, restar, multiplicar y dividir, La funcion 
+debe recibir 3 parametros 
+*/
